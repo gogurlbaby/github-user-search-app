bbce82f583e6c7e236edac8fe6c6ba201a9fbbdd
phillybebe <philia.hammond@eggseed.com>
2023-10-09 19:17:38 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js}"],
+  theme: {
+    fontFamily: {
+      sans: ["sans-serif"],
+      serif: ["Manrope", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
